cc56dd4638cd87f9080098075b937183fbb55a23
sivanandan <sivanandan@IBM656-PC0ER3LV.yellowdc.com>
2019-01-10 20:39:32 +0530

    sdfghj

---
+++ ABC/src/Student.java
@@ -0,0 +1,29 @@
+
+public class Student {
+	private String name;
+	private Integer id;
+	private String mail;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", id=" + id + ", mail=" + mail + "]";
+	}
+	
+}
